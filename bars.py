ec8eff08599fda5a378ee0b88593f445117d393e
manchos <vladm.ru@gmail.com>
2016-09-28 23:14:13 +0300

    upgrade 1

--- bars.py
+++ bars.py
@@ -4,51 +4,46 @@ from math import sin, cos, sqrt, atan2, radians
 
 
 def load_data(filepath):
-	if os.path.exists(filepath):
-		with open(filepath, encoding = 'utf8' ) as data_file:
-			data = json.load(data_file)
-			return data
-	else:
-		return 'file not found'
+	if not os.path.exists(filepath):
+		return None
+	with open(filepath, encoding = 'utf8' ) as file_handler:
+		return json.load(file_handler)
 
 
 
 
-def get_biggest_bar(data):
-	m = data[0]
-	for i in data:
-		if i['Cells']['SeatsCount'] > m['Cells']['SeatsCount']:
-			m = i
-	return m
+def get_biggest_bar(bars):
+	big_bar = bars[0]
+	for bar in bars:
+		if bar['Cells']['SeatsCount'] > big_bar['Cells']['SeatsCount']:
+			big_bar = bar
+	return big_bar
 
 
-def get_smallest_bar(data):
-	m = data[0]
-	for i in data:
-		if i['Cells']['SeatsCount'] < m['Cells']['SeatsCount']:
-			m = i
-	return m
+def get_smallest_bar(bars):
+	small_bar = bars[0]
+	for bar in bars:
+		if bar['Cells']['SeatsCount'] < small_bar['Cells']['SeatsCount']:
+			small_bar = bar
+	return small_bar
 
 
 def get_closest_bar(data, lon1, lat1):
-    cl = data[0]
-    distance_min = distance(lon1, lat1, cl['Cells']['geoData']['coordinates'][0], cl['Cells']['geoData']['coordinates'][1])
-    for i in data:
-    	distance_cur = distance(lon1, lat1, i['Cells']['geoData']['coordinates'][0], i['Cells']['geoData']['coordinates'][1])
+    closest_bar = data[0]
+    distance_min = distance(lon1, lat1, closest_bar['Cells']['geoData']['coordinates'][0], closest_bar['Cells']['geoData']['coordinates'][1])
+    for bar in data:
+    	distance_cur = distance(lon1, lat1, bar['Cells']['geoData']['coordinates'][0], bar['Cells']['geoData']['coordinates'][1])
     	if distance_min > distance_cur:
     		distance_min = distance_cur
-    		#print(distance_min)
-    		#print(i['Cells']['Name'])
-    		cl = i
-    return cl    
+    		closest_bar  = bar
+    return closest_bar     
 
 def distance(lon1, lat1, lon2, lat2):
-	R = 6373.0
+	earth_radius = 6373.0
 	dlon = lon2 - lon1
 	dlat = lat2 - lat1
-	a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-	c = 2 * atan2(sqrt(a), sqrt(1 - a))
-	return R * c
+	a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2	
+	return earth_radius * c * 2 * atan2(sqrt(a), sqrt(1 - a))
 
 
 if __name__ == '__main__':
