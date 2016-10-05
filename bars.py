9e48aca2b2d619d89f1bc07f030b0b1579402a07
manchos <vladm.ru@gmail.com>
2016-10-06 01:06:31 +0400

    best solution

--- bars.py
+++ bars.py
@@ -4,59 +4,56 @@ from math import sin, cos, sqrt, atan2, radians
 
 
 def load_data(filepath):
-	if os.path.exists(filepath):
-		with open(filepath, encoding = 'utf8' ) as data_file:
-			data = json.load(data_file)
-			return data
-	else:
-		return 'file not found'
+    """Return the list of bars."""
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, encoding = 'utf8' ) as file_handler:
+        return json.load(file_handler)
 
 
+def get_biggest_bar(bars):
+    """Return the biggest bar from the list of bars."""
+    big_bar = bars[0]
+    for bar in bars:
+        if bar['Cells']['SeatsCount'] > big_bar['Cells']['SeatsCount']:
+            big_bar = bar
+    return big_bar
 
 
-def get_biggest_bar(data):
-	m = data[0]
-	for i in data:
-		if i['Cells']['SeatsCount'] > m['Cells']['SeatsCount']:
-			m = i
-	return m
-
-
-def get_smallest_bar(data):
-	m = data[0]
-	for i in data:
-		if i['Cells']['SeatsCount'] < m['Cells']['SeatsCount']:
-			m = i
-	return m
+def get_smallest_bar(bars):
+    """Return the smallest bar from the list of bars."""
+    small_bar = bars[0]
+    for bar in bars:
+        if bar['Cells']['SeatsCount'] < small_bar['Cells']['SeatsCount']:
+            small_bar = bar
+    return small_bar
 
 
 def get_closest_bar(data, lon1, lat1):
-    cl = data[0]
-    distance_min = distance(lon1, lat1, cl['Cells']['geoData']['coordinates'][0], cl['Cells']['geoData']['coordinates'][1])
-    for i in data:
-    	distance_cur = distance(lon1, lat1, i['Cells']['geoData']['coordinates'][0], i['Cells']['geoData']['coordinates'][1])
-    	if distance_min > distance_cur:
-    		distance_min = distance_cur
-    		print(distance_min)
-    		print(i['Cells']['Name'])
-    		cl = i
-    return cl    
+    """Return the closest bar from the list of bars given the GPS coordinates."""
+    closest_bar = data[0]
+    distance_min = distance(lon1, lat1, closest_bar['Cells']['geoData']['coordinates'][0], closest_bar['Cells']['geoData']['coordinates'][1])
+    for bar in data:
+        distance_cur = distance(lon1, lat1, bar['Cells']['geoData']['coordinates'][0], bar['Cells']['geoData']['coordinates'][1])
+        if distance_min > distance_cur:
+            distance_min = distance_cur
+            closest_bar  = bar
+    return closest_bar
+
 
 def distance(lon1, lat1, lon2, lat2):
-	R = 6373.0
-	dlon = lon2 - lon1
-	dlat = lat2 - lat1
-	a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-	c = 2 * atan2(sqrt(a), sqrt(1 - a))
-	return R * c
-
+    """Return the distance between the points"""
+    earth_radius = 6373.0
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    return earth_radius * c * 2 * atan2(sqrt(a), sqrt(1 - a))
 
 if __name__ == '__main__':
-    filepath = 'E:\\python\\projects\\devmanorg\\3_bars\\Бары.json'
-    data = load_data(filepath)
-    print(data[0]['Cells']['Name'])
-    print(get_biggest_bar(data))
-    print(get_smallest_bar(data))
-    lon1, lat1 = 37.439787, 55.85051 # метро Сходненская
-    print(get_closest_bar(data, lon1, lat1))
+    filepath = input('Enter the real path to json file with list of bars:')
+    bars = load_data(filepath)
+    print('The biggest bar: %s' % get_biggest_bar(bars))
+    print('The smallest bar: %s' % get_smallest_bar(bars))
+    lon1, lat1 = 37.439787, 55.85051 # metro station Skhodnenskaya
+    print('the closest bar near the metro station Skhodnenskaya: %s' % get_closest_bar(bars, lon1, lat1))
 
