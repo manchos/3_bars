de84bcc8a442c88afb9546d2fe34acaa5171d57a
manchos <vladm.ru@gmail.com>
2016-10-06 01:24:50 +0400

    SOlving 3

--- bars.py
+++ bars.py
@@ -4,18 +4,11 @@ from math import sin, cos, sqrt, atan2, radians
 
 
 def load_data(filepath):
-<<<<<<< HEAD
     """Return the list of bars."""
     if not os.path.exists(filepath):
         return None
     with open(filepath, encoding = 'utf8' ) as file_handler:
         return json.load(file_handler)
-=======
-	if not os.path.exists(filepath):
-		return None
-	with open(filepath, encoding = 'utf8' ) as file_handler:
-		return json.load(file_handler)
->>>>>>> ec8eff08599fda5a378ee0b88593f445117d393e
 
 
 def get_biggest_bar(bars):
@@ -27,7 +20,6 @@ def get_biggest_bar(bars):
     return big_bar
 
 
-<<<<<<< HEAD
 def get_smallest_bar(bars):
     """Return the smallest bar from the list of bars."""
     small_bar = bars[0]
@@ -48,40 +40,6 @@ def get_closest_bar(data, lon1, lat1):
             closest_bar  = bar
     return closest_bar
 
-=======
-def get_biggest_bar(bars):
-	big_bar = bars[0]
-	for bar in bars:
-		if bar['Cells']['SeatsCount'] > big_bar['Cells']['SeatsCount']:
-			big_bar = bar
-	return big_bar
-
-
-def get_smallest_bar(bars):
-	small_bar = bars[0]
-	for bar in bars:
-		if bar['Cells']['SeatsCount'] < small_bar['Cells']['SeatsCount']:
-			small_bar = bar
-	return small_bar
-
-
-def get_closest_bar(data, lon1, lat1):
-    closest_bar = data[0]
-    distance_min = distance(lon1, lat1, closest_bar['Cells']['geoData']['coordinates'][0], closest_bar['Cells']['geoData']['coordinates'][1])
-    for bar in data:
-    	distance_cur = distance(lon1, lat1, bar['Cells']['geoData']['coordinates'][0], bar['Cells']['geoData']['coordinates'][1])
-    	if distance_min > distance_cur:
-    		distance_min = distance_cur
-    		closest_bar  = bar
-    return closest_bar     
-
-def distance(lon1, lat1, lon2, lat2):
-	earth_radius = 6373.0
-	dlon = lon2 - lon1
-	dlat = lat2 - lat1
-	a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2	
-	return earth_radius * c * 2 * atan2(sqrt(a), sqrt(1 - a))
->>>>>>> ec8eff08599fda5a378ee0b88593f445117d393e
 
 def distance(lon1, lat1, lon2, lat2):
     """Return the distance between the points"""
