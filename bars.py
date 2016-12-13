6a5494a00bb2c69aa8f65d7d7a46a3f5ba3afb1b
mancho <vladm.ru@gmail.com>
2016-12-13 20:36:05 +0300

    Update bars.py
    add the names

--- bars.py
+++ bars.py
@@ -4,25 +4,25 @@ from math import sin, cos, sqrt, atan2, radians
 
 
 def load_data(filepath):
-    """Return the list of bars."""
+    """Return the dictionary of bars."""
     if not os.path.exists(filepath):
         return None
     with open(filepath, encoding='utf8') as file_handler:
         return json.load(file_handler)
 
 
-def get_biggest_bar(bars):
-    """Return the biggest bar from the list of bars."""
+def get_biggest_bar_name(bars):
+    """Return the name of the biggest bar from the list of bars."""
     return max(bars, key=lambda x: x['Cells']['SeatsCount'])['Cells']['Name']
 
 
-def get_smallest_bar(bars):
-    """Return the smallest bar from the list of bars."""
+def get_smallest_bar_name(bars):
+    """Return the name of the smallest bar from the list of bars."""
     return min(bars, key=lambda x: x['Cells']['SeatsCount'])['Cells']['Name']
 
 
-def get_closest_bar(data, lon1, lat1):
-    """Return the closest bar from the list of bars given the GPS coordinates."""
+def get_closest_bar_name(data, lon1, lat1):
+    """Return the name of the closest bar from the list of bars given the GPS coordinates."""
     closest_bar = data[0]
     distance_min = distance(lon1, lat1, closest_bar['Cells']['geoData']['coordinates'][0],
                             closest_bar['Cells']['geoData']['coordinates'][1])
@@ -48,10 +48,10 @@ if __name__ == '__main__':
     filepath = input('Enter the real path to json file with list of bars:')
     bars = load_data(filepath)
     print('----------------------------------------------------')
-    print('The biggest bar: %s' % get_biggest_bar(bars))
-    print('The smallest bar: %s' % get_smallest_bar(bars))
+    print('The name of the biggest bar: %s' % get_biggest_bar_name(bars))
+    print('The smallest bar: %s' % get_smallest_bar_name(bars))
     print('----------------------------------------------------')
     lon1 = float(input('Enter the longitude:'))
     lat1 = float(input('Enter the latitude:'))
     # 37.439787, 55.85051  # metro station Skhodnenskaya
-    print('the closest bar near the longtitude: %f  latitude: %f: \n %s' % (lon1, lat1, get_closest_bar(bars, lon1, lat1)))
+    print('the name of the closest bar near the longtitude: %f  latitude: %f: \n %s' % (lon1, lat1, get_closest_bar(bars, lon1, lat1)))
