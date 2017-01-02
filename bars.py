72ab574712cc5a1b8b0f9657419543ae36d09b9d
manchos <vladm.ru@gmail.com>
2017-01-03 01:01:04 +0300

    removed Cells from json file, returned function distance and optimized get_closest_bar_name()

--- bars.py
+++ bars.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 from math import sin, cos, sqrt, atan2, radians
 import json
 import os
+
 
 def load_data(filepath):
     """Return the dictionary of bars."""
@@ -12,18 +14,27 @@ def load_data(filepath):
 
 def get_biggest_bar_name(bars):
     """Return the name of the biggest bar from the list of bars."""
-    return max(bars, key=lambda x: x['Cells']['SeatsCount'])['Cells']['Name']
+    return max(bars, key=lambda x: x['SeatsCount'])['Name']
 
 
 def get_smallest_bar_name(bars):
     """Return the name of the smallest bar from the list of bars."""
-    return min(bars, key=lambda x: x['Cells']['SeatsCount'])['Cells']['Name']
+    return min(bars, key=lambda x: x['SeatsCount'])['Name']
 
 
 def get_closest_bar_name(data, lon1, lat1):
     """Return the name of the closest bar from the list of bars given the GPS coordinates."""
-    return min(data, key=lambda x: 6373.0 * 2 * atan2(sqrt(sin((x['Cells']['geoData']['coordinates'][1] - lat1) / 2) ** 2 + cos(lat1) * cos(x['Cells']['geoData']['coordinates'][1]) * sin((x['Cells']['geoData']['coordinates'][0]- lon1) / 2) ** 2), sqrt(1 - sin((x['Cells']['geoData']['coordinates'][1] - lat1)/ 2) ** 2 + cos(lat1) * cos(x['Cells']['geoData']['coordinates'][1]) * sin((x['Cells']['geoData']['coordinates'][0] - lon1) / 2) ** 2)))['Cells']['Name']
+    return \
+        min(data,
+            key=lambda x: distance(lon1, lat1, x['geoData']['coordinates'][0], x['geoData']['coordinates'][1]))['Name']
 
+
+def distance(lon1, lat1, lon2, lat2):
+    earth_radius = 6373.0
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    return earth_radius * 2 * atan2(sqrt(a), sqrt(1 - a))
 
 
 if __name__ == '__main__':
@@ -36,4 +47,5 @@ if __name__ == '__main__':
     lon1 = float(input('Enter the longitude:'))
     lat1 = float(input('Enter the latitude:'))
     # 37.439787, 55.85051  # metro station Skhodnenskaya
-    print('the name of the closest bar near the longtitude: %f  latitude: %f: \n %s' % (lon1, lat1, get_closest_bar_name(bars, lon1, lat1)))
+    print('the name of the closest bar near the longtitude:' +
+          '%f  latitude: %f: \n %s' % (lon1, lat1, get_closest_bar_name(bars, lon1, lat1)))
